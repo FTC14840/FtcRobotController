675baf152f39e4669e4351821dce514eaebf14ff
jwbraun <jwbraun@decaturchristian.net>
2020-09-27 19:06:50 -0500

    Added Example class to each folder.  Empty folders weren't pushing.

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeamMemberNoah/Example.java
@@ -0,0 +1,4 @@
+package org.firstinspires.ftc.teamcode.TeamMemberNoah;
+
+public class Example {
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeamMemberSam/Example.java
@@ -0,0 +1,4 @@
+package org.firstinspires.ftc.teamcode.TeamMemberSam;
+
+public class Example {
+}
