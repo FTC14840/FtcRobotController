d4672862f7df308d15468c094165f75d4e8feaca
tatemherdina <tatemgrams@gmail.com>
2020-09-30 13:11:46 -0500

    Little overview

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeamMemberTatem/TeleOpMecanumExample.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeamMemberTatem/TeleOpMecanumExample.java
@@ -6,6 +6,7 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.util.Range;
 
+//changes
 // Name under TeleOp List
 @TeleOp(name ="TeleOpMecanumExample")
 
