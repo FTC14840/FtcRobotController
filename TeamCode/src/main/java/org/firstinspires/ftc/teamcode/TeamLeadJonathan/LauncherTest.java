2d84723384611cf2dabdb14094d043b4eecf5390
LAPTOP-SJPMJEBR\johny <johnybobaustin@gmail.com>
2020-10-05 22:05:05 -0500

    Launcher Test program from scratch

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeamLeadJonathan/LauncherTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeamLeadJonathan/LauncherTest.java
@@ -1,37 +1,45 @@
 package org.firstinspires.ftc.teamcode.TeamLeadJonathan;
 
+
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
-import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.util.ElapsedTime;
+import com.qualcomm.robotcore.util.Range;
 
-import static com.qualcomm.robotcore.hardware.DcMotorSimple.Direction.FORWARD;
-import static org.firstinspires.ftc.robotcore.external.BlocksOpModeCompanion.opMode;
 
-@TeleOp(name="Jonathan Launcher Test")
+
+@TeleOp(name="Launcher Test")
 
 public class LauncherTest extends LinearOpMode {
-    private DcMotor backTestLauncher;
-    private DcMotor frontTestLauncher;
 
+    // Declare OpMode members.
+    private DcMotor backLauncher = null;
+    private DcMotor frontLauncher = null;
 
     @Override
-    public void runOpMode() throws InterruptedException {
+    public void runOpMode() {
+        telemetry.addData("Status", "Initialized");
+        telemetry.update();
 
-        backTestLauncher = opMode.hardwareMap.get(DcMotor.class, "backLauncher");
-        frontTestLauncher = opMode.hardwareMap.get(DcMotor.class, "frontLauncher");
-        backTestLauncher.setDirection(FORWARD);
-        frontTestLauncher.setDirection(FORWARD);
+        // Initialize the hardware variables. Note that the strings used here as parameters
+        // to 'get' must correspond to the names assigned during the robot configuration
+        // step (using the FTC Robot Controller app on the phone).
+        backLauncher = hardwareMap.get(DcMotor.class, "backLauncher");
+        frontLauncher = hardwareMap.get(DcMotor.class, "frontLauncher");
 
-        waitForStart();
+        // Most robots need the motor on one side to be reversed to drive forward
+        // Reverse the motor that runs backwards when connected directly to the battery
+        backLauncher.setDirection(DcMotor.Direction.FORWARD);
+        frontLauncher.setDirection(DcMotor.Direction.FORWARD);
 
+
+        // run until the end of the match (driver presses STOP)
         while (opModeIsActive()) {
-            backTestLauncher.setPower(1.0);
-            frontTestLauncher.setPower(1.0);
-
-
+            backLauncher.setPower(1);
+            frontLauncher.setPower(1);
         }
     }
 }
-
