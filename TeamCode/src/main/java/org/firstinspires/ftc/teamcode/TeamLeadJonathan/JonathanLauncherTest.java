4537d1b121f283530254e7d101fbbbdc772687bb
DC\JonathanAustin <johnybobaustin@gmail.com>
2020-10-26 10:35:58 -0500

    Still Working on launcher

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeamLeadJonathan/JonathanLauncherTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeamLeadJonathan/JonathanLauncherTest.java
@@ -9,13 +9,15 @@ import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.util.ElapsedTime;
 import com.qualcomm.robotcore.util.Range;
 
+import static com.qualcomm.robotcore.hardware.DcMotor.RunMode.RUN_USING_ENCODER;
+import static com.qualcomm.robotcore.hardware.DcMotor.RunMode.RUN_WITHOUT_ENCODER;
+import static com.qualcomm.robotcore.hardware.DcMotor.RunMode.STOP_AND_RESET_ENCODER;
 import static com.qualcomm.robotcore.hardware.DcMotorSimple.Direction.FORWARD;
 import static com.qualcomm.robotcore.hardware.DcMotorSimple.Direction.REVERSE;
 
 
 @TeleOp(name="Jonathan Launcher Test")
 
-@Disabled
 
 public class JonathanLauncherTest extends LinearOpMode {
 
@@ -24,6 +26,7 @@ public class JonathanLauncherTest extends LinearOpMode {
     private DcMotor frontLauncher = null;
     private static final double INCREMENT1 = 0.001;
     private static final double INCREMENT2 = 0.005;
+
 
 
 
@@ -44,36 +47,51 @@ public class JonathanLauncherTest extends LinearOpMode {
         waitForStart();
         backLauncher.setDirection(REVERSE);
         frontLauncher.setDirection(REVERSE);
-        backLauncher.setPower(.80);
-        frontLauncher.setPower(.80);
+        backLauncher.setMode(STOP_AND_RESET_ENCODER);
+        frontLauncher.setMode(STOP_AND_RESET_ENCODER);
+
+        backLauncher.setPower(1.0);
+        frontLauncher.setPower(1.0);
 
 
         // run until the end of the match (driver presses STOP)
         while (opModeIsActive()) {
-            telemetry.log().clear();
-            telemetry.addData("Encoder", "Launcher: %2d", frontLauncher.getCurrentPosition());
-            telemetry.addData("Encoder", "Launcher: %2d", backLauncher.getCurrentPosition());
-            telemetry.addData("Power", "Launcher: %.2f,", frontLauncher.getPower());
-            telemetry.update();
+            backLauncher.setMode(RUN_USING_ENCODER);
+            frontLauncher.setMode(RUN_USING_ENCODER);
 
             if (gamepad1.dpad_down) {
                 backLauncher.setPower(backLauncher.getPower() - INCREMENT1);
                 frontLauncher.setPower(frontLauncher.getPower() - INCREMENT1);
+
+                telemetry.update();
             }
+
             if (gamepad1.dpad_up) {
                 backLauncher.setPower(backLauncher.getPower() + INCREMENT1);
                 frontLauncher.setPower(frontLauncher.getPower() + INCREMENT1);
+                telemetry.update();
             }
-            if (gamepad1.dpad_left){
+            if (gamepad1.dpad_left) {
                 backLauncher.setPower(backLauncher.getPower() - INCREMENT2);
                 frontLauncher.setPower(frontLauncher.getPower() - INCREMENT2);
+                telemetry.update();
             }
-            if (gamepad1.dpad_right){
+            if (gamepad1.dpad_right) {
                 backLauncher.setPower(backLauncher.getPower() + INCREMENT2);
                 frontLauncher.setPower(frontLauncher.getPower() + INCREMENT2);
+                telemetry.update();
             }
+
+            telemetry.log().clear();
+            telemetry.addData("BackEncoder", "Launcher: %2d", frontLauncher.getCurrentPosition());
+            telemetry.addData("FrontEncoder", "Launcher: %2d", backLauncher.getCurrentPosition());
+            telemetry.addData("FrontPower", "Launcher: %.2f,", frontLauncher.getPower());
+            telemetry.addData("BackPower", "Launcher: %.2f,", backLauncher.getPower());
+            telemetry.update();
         }
+
         backLauncher.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.FLOAT);
         frontLauncher.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.FLOAT);
     }
 }
+
