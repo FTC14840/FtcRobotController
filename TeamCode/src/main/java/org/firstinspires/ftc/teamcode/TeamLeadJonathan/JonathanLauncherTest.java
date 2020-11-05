25dd5dfb2b712147e053be2ec31d485d428f4844
ftc14840 <ftc14840@decaturchristian.net>
2020-11-05 13:33:39 -0600

    no message

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeamLeadJonathan/JonathanLauncherTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeamLeadJonathan/JonathanLauncherTest.java
@@ -4,7 +4,6 @@ package org.firstinspires.ftc.teamcode.TeamLeadJonathan;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.util.Range;
 
 import static com.qualcomm.robotcore.hardware.DcMotor.RunMode.RUN_USING_ENCODER;
@@ -22,12 +21,11 @@ public class JonathanLauncherTest extends LinearOpMode {
     private DcMotor leftLauncher;
     private DcMotor rightLauncher;
 
-    double launcherIncrement = 30.73;
-    double launcherPower = .001;
+    double launcherPower = 0.9;
 
-    public void runOpMode() throws InterruptedException {
-        leftLauncher = hardwareMap.get(DcMotorEx.class, "backLauncher");
-        rightLauncher = hardwareMap.get(DcMotorEx.class, "frontLauncher");
+    public void runOpMode() throws InterruptedException{
+        leftLauncher = hardwareMap.get(DcMotor.class, "backLauncher");
+        rightLauncher = hardwareMap.get(DcMotor.class, "frontLauncher");
         telemetry.addData("Revving Up", "Prepare to Fire");
         telemetry.update();
         //Init specific launcher hardware for less confusion
@@ -37,41 +35,48 @@ public class JonathanLauncherTest extends LinearOpMode {
         rightLauncher.setMode(STOP_AND_RESET_ENCODER);
         leftLauncher.setMode(RUN_WITHOUT_ENCODER);
         rightLauncher.setMode(RUN_WITHOUT_ENCODER);
-//        leftLauncher.setMode(RUN_USING_ENCODER);
-//        rightLauncher.setMode(RUN_USING_ENCODER);
         leftLauncher.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.FLOAT);
         rightLauncher.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.FLOAT);
-//        leftLauncher.setVelocity(0.0);
-//        rightLauncher.setVelocity(0.0);
-        leftLauncher.setPower(0);
-        rightLauncher.setPower(0);
+        leftLauncher.setPower(0.0);
+        rightLauncher.setPower(0.0);
+
 
         waitForStart();
-        leftLauncher.setPower(.9);
-        rightLauncher.setPower(.9);
-//        leftLauncher.setVelocity(3073.26);
-//        rightLauncher.setVelocity(3073.26);
 
+        leftLauncher.setPower(launcherPower);
+        rightLauncher.setPower(launcherPower);
 
         while (opModeIsActive()) {
+
+            if (gamepad1.dpad_up && launcherPower <= 1.0) {
+                launcherPower = launcherPower + .001;
+            }
+
+            if (gamepad1.dpad_down && launcherPower >= 0.0) {
+                launcherPower = launcherPower - .001;
+            }
+
+            if (gamepad1.dpad_right) {
+                launcherPower = 1.0;
+            }
+
+            if (gamepad1.dpad_left) {
+                launcherPower = 0.0;
+            }
+
+//            launcherPower = Range.clip(leftLauncher.getPower(), 0, 1);
+//            launcherPower = Range.clip(rightLauncher.getPower(), 0, 1);
+            leftLauncher.setPower(launcherPower);
+            rightLauncher.setPower(launcherPower);
+
+
+//            if (gamepad1.dpad_down) {
+//                rightLauncher.setPower(rightLauncher.getPower() - INCREMENT1);
+//                leftLauncher.setPower(leftLauncher.getPower() - INCREMENT1);
+//            }
 //            if (gamepad1.dpad_up) {
-//                leftLauncher.setVelocity(leftLauncher.getVelocity()+launcherIncrement);
-//                rightLauncher.setVelocity(rightLauncher.getVelocity()+launcherIncrement);
-//            }
-//            if (gamepad1.dpad_down){
-//                leftLauncher.setVelocity(leftLauncher.getVelocity()-launcherIncrement);
-//                rightLauncher.setVelocity(rightLauncher.getVelocity()-launcherIncrement);
-//            }
-
-
-            if (gamepad1.dpad_down) {
-                rightLauncher.setPower(rightLauncher.getPower() - launcherPower);
-                leftLauncher.setPower(leftLauncher.getPower() - launcherPower);
-            }
-            if (gamepad1.dpad_up) {
-                rightLauncher.setPower(rightLauncher.getPower() + launcherPower);
-                leftLauncher.setPower(leftLauncher.getPower() + launcherPower);
-            }
+//                rightLauncher.setPower(rightLauncher.getPower() + INCREMENT1);
+//                leftLauncher.setPower(leftLauncher.getPower() + INCREMENT1);
 //            }
 //            if (gamepad1.dpad_left) {
 //                rightLauncher.setPower(Range.clip(rightLauncher.getPower() - INCREMENT2, 0, 1));
@@ -97,17 +102,12 @@ public class JonathanLauncherTest extends LinearOpMode {
 //                leftLauncher.setPower(0.4);
 //                rightLauncher.setPower(0.4);
 //            }
-//            telemetry.log().clear();
-////            telemetry.addData("FrontPower", " %.2f,", rightLauncher.getVelocity());
-////            telemetry.addData("BackPower", " %.2f,", leftLauncher.getVelocity());
-////            telemetry.addData("LauncherPower", " %.2f,", launcherIncrement);
-////            telemetry.update();
+            telemetry.log().clear();
+            telemetry.addData("FrontPower", " %.2f,", rightLauncher.getPower());
+            telemetry.addData("BackPower", " %.2f,", leftLauncher.getPower());
+            telemetry.addData("LauncherPower", " %.2f,", launcherPower);
+            telemetry.update();
         }
-        telemetry.log().clear();
-        telemetry.addData("FrontPower", " %.2f,", rightLauncher.getPower());
-        telemetry.addData("BackPower", " %.2f,", leftLauncher.getPower());
-        telemetry.addData("LauncherPower", " %.2f,", launcherPower);
-        telemetry.update();
     }
 }
 
